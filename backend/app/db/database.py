db77daea1386773ed388ae178a55ab66428794d7
eswm223-oss <eswm223@gmail.com>
2026-07-16 22:26:40 +0900

    Update database.py

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
+from collections.abc import Generator
 
 engine = create_engine(
     settings.database_url,
@@ -13,3 +14,10 @@ SessionLocal = sessionmaker(
     autoflush=False,
     bind=engine,
 )
+
+def get_db() -> Generator[Session, None, None]:
+    db = sessionmaker()
+    try:
+        yield db
+    finally:
+        db.close
